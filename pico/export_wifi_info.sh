7020dac17a6dd42bdfdf21ab2aa7b4a12947b8b2
Michal <mikh3x4@gmail.com>
2023-06-23 04:54:47 -0700

    wifi

---
+++ pico/export_wifi_info.sh
@@ -0,0 +1,5 @@
+
+
+export WIFI_SSID="my_ssid"
+export WIFI_PASSWORD="my_pass"
+
--- pico/main.c
+++ pico/main.c
@@ -32,10 +32,48 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#include "pico/cyw43_arch.h"
+
+#include "lwip/pbuf.h"
+#include "lwip/udp.h"
+
+#define UDP_PORT 4444
+#define BEACON_MSG_LEN_MAX 127
+#define BEACON_TARGET "255.255.255.255"
+#define BEACON_INTERVAL_MS 1000
+
+
 int main() {
     stdio_init_all();
+
+    if (cyw43_arch_init()) {
+        printf("failed to initialise\n");
+        return 1;
+    }
+    cyw43_arch_enable_sta_mode();
+
+    printf("Connecting to Wi-Fi...\n");
+    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+        printf("failed to connect.\n");
+        return 1;
+    } else {
+        printf("Connected.\n");
+    }
+
+
+
     while (true) {
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
 }
+
+
+
+
+
+
+
+
+
+
