55be1a770841f278216d699830fb6f3aad81e600
Michal <mikh3x4@gmail.com>
2023-06-25 04:30:30 -0700

    explaiton

--- pico/wifi_pass.h
+++ pico/wifi_pass.h
@@ -1,4 +1,8 @@
 
+//Put your password here
+//you can use
+//git update-index --assume-unchanged wifi_pass.h
+//to prevent git from buging you that it is changed
 
 #define WIFI_SSID "MySSID"
 #define WIFI_PASSWORD "MyPassword"
