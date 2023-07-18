a873b9764668681abbee2e8272c2672c45ed0053
Michal <mikh3x4@gmail.com>
2023-07-18 02:17:19 +0200

    added just in case

--- controller/install.sh
+++ controller/install.sh
@@ -1,5 +1,10 @@
 
+# export EDITOR=nano
+# alias backup='git add * && git commit'
+# alias update='git -C /home/smartup/smartup-robot pull & cp /home/smartup/smartup-robot/controller/example.py /home/smartup/Desktop/SmartUp-code/example.py'
+# export PATH=~/apps/thonny/bin:$PATH
+
 echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="0810", MODE="0666", GROUP="plugdev"' | sudo tee /etc/udev/rules/99-my-device.rules
-pip install hidapi
-pip install -e .
+pip3 install hidapi
+pip3 install -e .
 
