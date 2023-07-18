5453bf52bc22084f02be27e53dfbf8e9a9e9c370
Michal <mikh3x4@gmail.com>
2023-07-19 01:18:47 +0200

    removed test code

--- pico/main.cpp
+++ pico/main.cpp
@@ -86,32 +86,33 @@ int main() {
     servos.init();
 
     multicore_launch_core1(core1_entry);
-    motor_2.driving_mode=MOTOR_MODE::DISTANCE;
-    uint8_t i;
-    while(1)
-    {
-        i=(i+1)%4;
-        switch(i)
-        {
-        case 0:
-            motor_2.drive_distance(20000,1023);
-        break;
-        case 1:
-            motor_2.drive_distance(-20000,1023);
-        break;
-        case 2:
-            motor_2.drive_distance(20000,255);
-        break;
-        case 3:
-            motor_2.drive_distance(-20000,255);
-        break;
-        }
-        motor_1.drive_power(300);
-        motor_3.drive_power(300);
-        motor_4.drive_power(300);
-        //printf("encoder status: %d %d %d %d\n",motor_1.encoder.get_count(),motor_2.encoder.get_count(),motor_3.encoder.get_count(),motor_4.encoder.get_count());
-        sleep_ms(10000);
-    }
+
+    // motor_2.driving_mode=MOTOR_MODE::DISTANCE;
+    // uint8_t i;
+    // while(1)
+    // {
+    //     i=(i+1)%4;
+    //     switch(i)
+    //     {
+    //     case 0:
+    //         motor_2.drive_distance(20000,1023);
+    //     break;
+    //     case 1:
+    //         motor_2.drive_distance(-20000,1023);
+    //     break;
+    //     case 2:
+    //         motor_2.drive_distance(20000,255);
+    //     break;
+    //     case 3:
+    //         motor_2.drive_distance(-20000,255);
+    //     break;
+    //     }
+    //     motor_1.drive_power(300);
+    //     motor_3.drive_power(300);
+    //     motor_4.drive_power(300);
+    //     //printf("encoder status: %d %d %d %d\n",motor_1.encoder.get_count(),motor_2.encoder.get_count(),motor_3.encoder.get_count(),motor_4.encoder.get_count());
+    //     sleep_ms(10000);
+    // }
 
     while (1) {
         main_data.telemetry.clear_debug();
