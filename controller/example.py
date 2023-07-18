27f407c87d947a7140e7e4b35a03d5f44188fa32
Michal <mikh3x4@gmail.com>
2023-07-18 08:08:59 +0200

    todo example

--- controller/example.py
+++ controller/example.py
@@ -15,8 +15,7 @@ from time import sleep
 gamepad = Gamepad()
 
 # Sprawdzcie IP swojego robota!
-# robot = Robot('192.168.1.10')
-robot = Robot('172.20.10.13')
+robot = Robot('192.168.1.10')
 
 while 1:
     sleep(0.05)
@@ -27,32 +26,20 @@ while 1:
         robot.estop()
         continue
 
-    # print(commands)
+    print(commands)
 
     robot.start()
 
-    if commands["r1"]:
-        front = commands["ry"]
-        side = commands["rx"]
-    else:
-        front = 0
-        side = 0
+    print(robot.get())
 
-    if commands["button_1"]:
+    if commands["r1"]:
+        robot.set_motor_power(2, 1024)
         robot.set_servo(1, 1500)
     else:
+        robot.set_motor_power(2, 0)
         robot.set_servo(1, 1800)
-
-    right = map(-front + side, -1, 1, -1023, 1023)
-    left = map(-front - side, -1, 1, -1023, 1023)
-
-    print(left, right)
-
-    robot.set_motor_power(2, right)
-    robot.set_motor_power(3, left)
 
     robot.set_led(0,255,0, 0)
 
-    print(robot.get())
 
 
