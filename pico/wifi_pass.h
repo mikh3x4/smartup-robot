33a96abd266d3ac161453ef93dcb36c49722c629
Michal <mikh3x4@gmail.com>
2023-07-19 15:01:36 +0200

    added copy

--- pico/wifi_pass.h
+++ pico/wifi_pass.h
@@ -4,5 +4,5 @@
 //git update-index --assume-unchanged wifi_pass.h
 //to prevent git from buging you that it is changed
 
-#define WIFI_SSID "MySSID"
-#define WIFI_PASSWORD "MyPassword"
+#define WIFI_SSID "smartUp"
+#define WIFI_PASSWORD "roboty!!"
