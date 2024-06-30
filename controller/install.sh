c2e8962aa283a0fdaf7001b5b55f9c2c40fb0de0
Michal <mikh3x4@gmail.com>
2024-06-30 02:18:29 +0200

    new install

--- controller/install.sh
+++ controller/install.sh
@@ -1,10 +1,18 @@
 
-# export EDITOR=nano
-# alias backup='git add * && git commit'
-# alias update='git -C /home/smartup/smartup-robot pull & cp /home/smartup/smartup-robot/controller/example.py /home/smartup/Desktop/SmartUp-code/example.py'
-# export PATH=~/apps/thonny/bin:$PATH
 
-echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="0810", MODE="0666", GROUP="plugdev"' | sudo tee /etc/udev/rules/99-my-device.rules
-pip3 install hidapi
-pip3 install -e .
+# put into .bashrc
+# export EDITOR=nano
+# alias backup='pushd ~/Desktop/SmartUp-code && git add * && git commit && popd'
+# alias update='git -C /home/smartup/smartup-robot pull && cp /home/smartup/smartup-robot/controller/example.py /home/smartup/Desktop/SmartUp-code/example.py'
+# export PATH=~/py_venv/bin:$PATH
+
+
+echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="0810", MODE="0666", GROUP="plugdev"' | sudo tee /etc/udev/rules.d/99-gamepad.rules
+
+pushd ~
+python3 -m venv py_venv --system-site-packages
+popd
+
+~/py_venv/bin/pip3 install hidapi
+~/py_venv/bin/pip3 install -e .
 
